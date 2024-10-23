b78dc2c0d2e021d441b2f1d83f5ea4a0887b3018
nikitapatil9994 <167848650+nikitapatil9994@users.noreply.github.com>
2024-10-23 20:10:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Count from './compo/Count';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Count/>
+      
+    </div>
+  );
+}
+
+export default App;
